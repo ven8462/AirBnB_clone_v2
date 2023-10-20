308129e79d1565e8fbd6879378120a7bd9e121fa
lavender <lavenderanyango9@gmail.com>
2023-10-20 10:35:50 +0300

    python dynamic text

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """display HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """displays HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text=None):
+    """Dynamic inputed text: replace _ with space and show text"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    """Dynamic inputed text: replace _ with space and show text"""
+    return "Python {}".format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
