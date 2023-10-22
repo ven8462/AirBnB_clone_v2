0a79faa97d0a847ede30df82706c8e0fe9a2258c
lavender <lavenderanyango9@gmail.com>
2023-10-22 11:53:18 +0300

    improve engine

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
-"""a script that starts a Flask web application"""
+"""display Hello HBNB!"""
 
-
-from flask import Flask, render_template
+from flask import Flask
 from models import storage
 from models.state import State
+from flask import render_template
+
+
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def state_list():
-    """fetches data from the storage engine"""
+def states_list():
+    '''
+    script that starts a Flask web application:
+    '''
     states = storage.all(State)
-    return render_template("7-states_list.py", state=states)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def close_session():
-    """remove the current SQLAlchemy Session"""
+def close_session(self):
+    '''Removes current SQLAlchemy session'''
     storage.close()
 
 
